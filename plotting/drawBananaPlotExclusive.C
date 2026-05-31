cc8e21f2ade001a3973e923cda21b33607021607
Hannah <hannah.bossi@yale.edu>
2026-05-31 19:19:18 +0200

    Add banana plot macros

---
+++ plotting/drawBananaPlotExclusive.C
@@ -0,0 +1,160 @@
+void drawBananaPlotExclusive(){
+    TFile* inFile = TFile::Open("BananaPlot_FSCCExclusive.root"); 
+    TH2D* bananaPlot = (TH2D*)inFile->Get("bananaPlot"); 
+    TH2D* bananaPlotFSCADC = (TH2D*)inFile->Get("bananaPlotFSCADC"); 
+    TH2D* bananaPlotFSCplus = (TH2D*)inFile->Get("bananaPlotFSCADCplus"); 
+    TH2D* bananaPlotFSCminus = (TH2D*)inFile->Get("bananaPlotFSCADCminus"); 
+    TH2D* zdcCorrMinus = (TH2D*)inFile->Get("zdcFSCcorr_minus"); 
+    TH2D* zdcCorrPlus = (TH2D*)inFile->Get("zdcFSCcorr_plus"); 
+
+
+
+    TLatex* cms = new TLatex(0.18,0.92,"#bf{CMS} #it{Preliminary} PbPb 5.36 TeV");
+    cms->SetNDC();
+    cms->SetTextSize(0.05);
+    cms->SetTextFont(42);
+
+    TLatex* eventSel = new TLatex(0.3,0.16,"Exclusive Event Selection");
+    eventSel->SetNDC();
+    eventSel->SetTextSize(0.035);
+    eventSel->SetTextFont(42);
+
+    // ------------------------------------------
+    /* ZDC Plus and Minus */
+    // ------------------------------------------
+
+
+   
+    
+    TCanvas* c3 = new TCanvas("c3","c3",600,600);
+    c3->cd();
+    c3->SetTickx(1);
+    c3->SetTicky(1);
+    c3->SetLogz(); 
+    c3->SetTopMargin(0.09);
+    c3->SetBottomMargin(0.08);
+    c3->SetLeftMargin(0.11);
+    c3->SetRightMargin(0.13);
+
+    //gStyle->SetPalette(kRainBow);
+
+    // zdc plus
+    bananaPlot->GetXaxis()->SetTitle("HF Transverse Energy Sum (GeV)"); 
+    bananaPlot->GetYaxis()->SetTitle("ZDC Energy Sum (GeV)"); 
+    bananaPlot->GetZaxis()->SetRangeUser(1.0, 30); 
+    bananaPlot->Draw("colz");
+    eventSel->Draw("same");  
+    cms->Draw("same");
+    c3->SaveAs("BananaPlotLHCC_Exclusive_ZDC.pdf");
+
+    TCanvas* c4 = new TCanvas("c4","c4",600,600);
+    c4->cd();
+    c4->SetTickx(1);
+    c4->SetTicky(1);
+    c4->SetLogz(); 
+    c4->SetTopMargin(0.09);
+    c4->SetBottomMargin(0.08);
+    c4->SetLeftMargin(0.13);
+    c4->SetRightMargin(0.13);
+
+    //gStyle->SetPalette(kRainBow);
+
+    // zdc plus
+    bananaPlotFSCADC->Rebin2D(1,5); 
+    bananaPlotFSCADC->GetXaxis()->SetTitle("HF Transverse Energy Sum (GeV)"); 
+    bananaPlotFSCADC->GetYaxis()->SetTitle("FSC ADC Sum "); 
+    bananaPlotFSCADC->GetZaxis()->SetRangeUser(1.0, 100); 
+    bananaPlotFSCADC->Draw("colz"); 
+    cms->Draw("same");
+    eventSel->Draw("same"); 
+    c4->SaveAs("BananaPlotFSC_Exclusive_LHCC.pdf");
+
+
+    TCanvas* c5= new TCanvas("c5","c5",600,600);
+    c5->cd();
+    c5->SetTickx(1);
+    c5->SetTicky(1);
+    c5->SetLogz(); 
+    c5->SetTopMargin(0.09);
+    c5->SetBottomMargin(0.08);
+    c5->SetLeftMargin(0.13);
+    c5->SetRightMargin(0.13);
+
+    //gStyle->SetPalette(kRainBow);
+
+    // zdc plus
+    bananaPlotFSCplus->Rebin2D(1,5); 
+    bananaPlotFSCplus->GetXaxis()->SetTitle("HF+ Transverse Energy Sum (GeV)"); 
+    bananaPlotFSCplus->GetYaxis()->SetTitle("FSC+ ADC Sum "); 
+    bananaPlotFSCplus->GetZaxis()->SetRangeUser(1.0, 100); 
+     bananaPlotFSCplus->GetYaxis()->SetRangeUser(0, 100);
+    bananaPlotFSCplus->Draw("colz"); 
+    cms->Draw("same");
+    eventSel->Draw("same"); 
+    c5->SaveAs("BananaPlotFSCplus_Exclusive_LHCC.pdf");
+    
+
+    TCanvas* c6= new TCanvas("c6","c6",600,600);
+    c6->cd();
+    c6->SetTickx(1);
+    c6->SetTicky(1);
+    c6->SetLogz(); 
+    c6->SetTopMargin(0.09);
+    c6->SetBottomMargin(0.08);
+    c6->SetLeftMargin(0.13);
+    c6->SetRightMargin(0.13);
+
+    //gStyle->SetPalette(kRainBow);
+
+    // zdc plus
+    bananaPlotFSCminus->Rebin2D(1,5); 
+    bananaPlotFSCminus->GetXaxis()->SetTitle("HF- Transverse Energy Sum (GeV)"); 
+    bananaPlotFSCminus->GetYaxis()->SetTitle("FSC- ADC Sum "); 
+    bananaPlotFSCminus->GetZaxis()->SetRangeUser(1.0, 100); 
+    bananaPlotFSCminus->Draw("colz"); 
+    cms->Draw("same");
+    eventSel->Draw("same"); 
+    c6->SaveAs("BananaPlotFSCminus_Exclusive_LHCC.pdf");
+
+
+    TCanvas* c7= new TCanvas("c7","c7",600,600);
+    c7->cd();
+    c7->SetTickx(1);
+    c7->SetTicky(1);
+    c7->SetLogz(); 
+    c7->SetTopMargin(0.09);
+    c7->SetBottomMargin(0.08);
+    c7->SetLeftMargin(0.13);
+    c7->SetRightMargin(0.13);
+
+    //gStyle->SetPalette(kRainBow);
+
+    // zdc plus
+    zdcCorrMinus->GetXaxis()->SetTitle("ZDC- Energy Sum (GeV)"); 
+    zdcCorrMinus->GetYaxis()->SetTitle("FSC- ADC Sum "); 
+    zdcCorrMinus->Draw("colz"); 
+    cms->Draw("same");
+    eventSel->Draw("same"); 
+    c7->SaveAs("BananaPlotZDCFDCCorrMinus_Exclusive_LHCC.pdf");
+
+    TCanvas* c8= new TCanvas("c8","c8",600,600);
+    c8->cd();
+    c8->SetTickx(1);
+    c8->SetTicky(1);
+    c8->SetLogz(); 
+    c8->SetTopMargin(0.09);
+    c8->SetBottomMargin(0.08);
+    c8->SetLeftMargin(0.13);
+    c8->SetRightMargin(0.13);
+
+    //gStyle->SetPalette(kRainBow);
+
+    // zdc plus
+    zdcCorrPlus->GetXaxis()->SetTitle("ZDC+ Energy Sum (GeV)"); 
+    zdcCorrPlus->GetYaxis()->SetTitle("FSC+ ADC Sum "); 
+    zdcCorrPlus->Draw("colz"); 
+    cms->Draw("same");
+    eventSel->Draw("same"); 
+    c8->SaveAs("BananaPlotZDCFDCCorrPlus_Exclusive_LHCC.pdf");
+    
+}
---
+++ plotting/plotBananaPlot.C
@@ -0,0 +1,210 @@
+//
+/*
+ * checkUnpacker.C: Macro to check that the unpacked sums and the emulated sums are equivalent.
+ * Input: Folder of L1Ntuples
+ * Output: A root file of histograms relevant to checking the unpacking. 
+ * Authors: Hannah Bossi <hannah.bossi@cern.ch>, Gian Michele Innocenti, <gian.michele.innocenti@cern.ch> 
+ * 9/10/23
+*/
+
+#include "TFile.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+#include "TTreeReaderArray.h"
+
+#include "TDirectory.h"
+#include "TSystemDirectory.h"
+#include "TSystemFile.h"
+#include "TChain.h"
+
+#include "TMath.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TGraphAsymmErrors.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+void GetFiles(char const *input, vector<string> &files) {
+  TSystemDirectory dir(input, input);
+  TList *list = dir.GetListOfFiles();
+
+  if (list) {
+    TSystemFile *file;
+    string fname;
+    TIter next(list);
+    while ((file = (TSystemFile *)next())) {
+      fname = file->GetName();
+
+      if (file->IsDirectory() && (fname.find(".") == string::npos)) {
+        string newDir = string(input) + fname + "/";
+        GetFiles(newDir.c_str(), files);
+      } else if ((fname.find(".root") != string::npos)) {
+        files.push_back(string(input) + fname);
+        cout << files.back() << endl;
+      }
+    }
+  }
+
+  return;
+}
+
+void FillChain(TChain &chain, vector<string> &files) {
+  for (auto file : files) {
+    chain.Add(file.c_str());
+  }
+}
+
+// params
+// input: folder of l1ntuples to read from - must contain the branches specified above
+// verbose: whether or not to print out the full output (error messgaes will always be printed)
+//input with fsc = /eos/cms/store/group/phys_heavyions/nbarnett/HiForest/DATA/2026PbPb/CRAB_UserFiles/crab_HiForest_run404350_RP_v5/260527_162844/0000/
+// inpu with zdc = /eos/cms/store/group/phys_heavyions/wangj/L1PbPb2026/HIPhysicsRawPrime/crab_forestadc_260524_PhysicsHIPhysicsRawPrime_404359_ZB_partial/260527_123027/0000/
+int plotBananaPlot(
+		  char const *input = "/eos/cms/store/group/phys_heavyions/nbarnett/HiForest/DATA/2026PbPb/CRAB_UserFiles/crab_HiForest_run404350_RP_v5/260527_162844/0000/") {
+
+    std::cout << "here" << std::endl;
+
+    gStyle->SetOptStat(0);
+    gStyle->SetOptTitle(0);
+
+    string tag = "Run404350"; 
+
+
+  /* read in all files in the input folder */
+  vector<string> files;
+  GetFiles(input, files);
+
+  /* read in the unpacked information */
+  TChain zdcChain("zdcanalyzer/zdcrechit");
+  FillChain(zdcChain, files);
+  TTreeReader zdcReader(&zdcChain);
+  TTreeReaderValue<float> sumPlus(zdcReader, "sumPlus");
+  TTreeReaderValue<float> sumMinus(zdcReader, "sumMinus");
+
+ TChain fscChain("fscanalyzer/fscdigi");
+  FillChain(fscChain, files);
+  TTreeReader fscReader(&fscChain);
+  TTreeReaderValue<float> fscSumPlus(fscReader, "sumPlus");
+  TTreeReaderValue<float> fscSumMinus(fscReader, "sumMinus");
+  TTreeReaderArray<int> adc(fscReader, "adcTs2"); 
+  TTreeReaderArray<int> ped(fscReader, "adcTs1"); 
+  TTreeReaderArray<int> zside(fscReader, "zside");
+
+    /* read in trigger information */
+  TChain trigChain("hltanalysis/HltTree");
+  FillChain(trigChain, files);
+  TTreeReader trigReader(&trigChain);
+  TTreeReaderValue<int> zb(trigReader, "L1_ZeroBias");
+  TTreeReaderValue<int> minBias(trigReader, "HLT_HIMinimumBiasHF1AND_v8"); 
+
+
+  /* read in the primary vertex filter*/
+  TChain vertexChain("skimanalysis/HltTree");
+  FillChain(vertexChain, files);
+  TTreeReader vertexReader(&vertexChain);
+  TTreeReaderValue<int> vertex(vertexReader, "pprimaryVertexFilter");
+  TTreeReaderValue<int> coincidence(vertexReader, "pphfCoincFilterPF2Th7"); 
+
+
+  /* read in the HF info */
+  TChain eventChain("hiEvtAnalyzer/HiTree");
+  FillChain(eventChain, files);
+  TTreeReader eventReader(&eventChain);
+  TTreeReaderValue<float> hfEsum(eventReader, "hiHF");
+  TTreeReaderValue<float>   zVertex(eventReader, "vz");
+  TTreeReaderValue<float> hFEsumPlus(eventReader, "hiHFplus"); 
+  TTreeReaderValue<float> hFEsumMinus(eventReader, "hiHFminus");
+
+  /* create histograms */
+  TH2D* bananaPlot         = new TH2D("bananaPlot", "", 100, 250, 7000, 1000, 0, 300000); 
+  TH2D* bananaPlotFSCADC   = new TH2D("bananaPlotFSCADC", "", 100, 0, 7000, 1000, 0, 3060); 
+  TH2D* bananaPlotFSCminus = new TH2D("bananaPlotFSCADCminus", "", 50, 0, 3500, 500, 0, 1530);
+  TH2D* bananaPlotFSCplus  = new TH2D("bananaPlotFSCADCplus", "", 50, 0, 3500, 500, 0, 1530);
+
+    
+
+  double maxZDC = -1; 
+  double maxHF = -1; 
+  double maxFSCADC = -1; 
+  Long64_t totalEvents = zdcReader.GetEntries(true);
+  for (Long64_t i = 0; i < totalEvents; i++) {
+    zdcReader.Next(); trigReader.Next(); vertexReader.Next(); eventReader.Next(); fscReader.Next(); 
+    
+    // zero bias selection
+    if(*zb!= 1) continue;
+
+    // primary vertex selection
+    if(*vertex != 1) continue; 
+
+    // coindidence filter selection 
+    if(*coincidence != 1) continue; 
+
+    // minimum bias selection 
+    if(*minBias != 1) continue; 
+
+    // z vertex selection 
+    if(*zVertex < -15.0 || *zVertex > 15.0) continue; 
+
+    if (i % 10000 == 0) {
+      cout << "Event " << i << endl;
+    }
+
+    double sumFSCADC   = 0; 
+    double sumFSCplus  = 0; 
+    double sumFSCminus = 0; 
+    for(int c = 0; c < 12; c++){
+        sumFSCADC += adc[c]; 
+        if(zside[c] < 0) sumFSCminus += adc[c]; 
+        if(zside[c] > 0) sumFSCplus += adc[c];
+    }   
+
+
+    double sum = *sumPlus + *sumMinus; 
+    double sumFSC = *fscSumPlus + *fscSumMinus;
+
+    if(sum > maxZDC) maxZDC = sum; 
+    if (*hfEsum > maxHF) maxHF = *hfEsum; 
+    if(sumFSCADC > maxFSCADC) maxFSCADC= sumFSCADC; 
+
+
+    //if(*hfEsum > 1000) std::cout << "ZDC sum: " << sum << " HF sum: " << *hfEsum << std::endl;
+
+    bananaPlot->Fill(*hfEsum, sum); 
+    bananaPlotFSCADC->Fill(*hfEsum, sumFSCADC); 
+    bananaPlotFSCplus->Fill(*hFEsumPlus, sumFSCplus);
+    bananaPlotFSCminus->Fill(*hFEsumMinus, sumFSCminus); 
+
+
+
+  } // end loop over  the  number of events
+
+    std::cout << "maxHF sum: " << maxHF << std::endl; 
+    std::cout << "maxZDC sum: " << maxZDC << std::endl; 
+    std::cout << "maxFSCADCSuM: " << maxFSCADC << std::endl;
+
+    TFile* outfile = new TFile("BananaPlot_FSCHadronic.root",  "RECREATE");
+    outfile->cd();
+    bananaPlot->Write(); 
+    bananaPlotFSCADC->Write();
+    bananaPlotFSCplus->Write(); 
+    bananaPlotFSCminus->Write(); 
+    outfile->Close();
+
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 2)
+    return plotBananaPlot(argv[1]);
+  else {
+    cout << "ERROR" << endl;
+    return -1;
+  }
+}
